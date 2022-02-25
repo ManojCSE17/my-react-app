78f63a66636748a565ef8d60683b4986d788a852
Manoj <manojcse1999@gmail.com>
2022-02-26 00:36:29 +0530

    feat: added assets and release

--- release.config.js
+++ release.config.js
@@ -5,6 +5,8 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/npm",
-        "@semantic-release/github"
+        ["@semantic-release/github", {
+            assets: [{path: "coverage.zip", label: "coverage (zip)"}]
+        }]
     ]
 }
