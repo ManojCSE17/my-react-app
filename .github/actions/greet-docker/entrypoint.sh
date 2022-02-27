14b037330c56751378015097906d282104cb50d1
Manoj <manojcse1999@gmail.com>
2022-02-27 12:13:38 +0530

    fix: patched Dockerfile again

--- .github/actions/greet-docker/entrypoint.sh
+++ .github/actions/greet-docker/entrypoint.sh
@@ -8,11 +8,12 @@ echo "::warning::This is a warning message"
 echo "::error::This is a error message"
 echo "::endgroup::"
 
-someMessage = "This is a secret"
+someMessage="This is a secret"
+
 echo "::add-mask::$someMessage"
 echo "Secret : $someMessage"
 
-todaytime = $(date)
+todaytime = $date
 echo "::set-output name=time-of-greet::$todaytime"
 
 echo "{'expvar'}={'This is a exported variable'}" >> $GITHUB_ENV
